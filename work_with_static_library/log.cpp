97ad0c4cd2cc2da44500cf885213a8ee63c87ad7
Mykola Solianko <mykola_solianko@epam.com>
2019-04-11 19:24:30 +0300

    Add info about library and code assemble

---
+++ work_with_static_library/log.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void log_error(const char* str) {
+  std::cerr << str << std::endl;
+}
+
+void log_info(const char* str) {
+  std::cerr << str << std::endl;
+}
+
+void log_debug(const char* str) {
+  std::cerr << str << std::endl;
+}
---
+++ work_with_static_library/log.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+void log_error(const char* str);
+
+void log_info(const char* str);
+
+void log_debug(const char* str);
